d3d4ef4f46540ec33457dd20f08fd73aad2c3d3f
Александр Базаров <120343545+AbOba506@users.noreply.github.com>
2023-04-26 20:39:19 +1000

    Update registration.py

--- registration.py
+++ registration.py
@@ -3,6 +3,108 @@ from tkinter import *
 from tkinter import messagebox
 import re
 import tkinter as tk
+from main import main
+from enter import ent
+
+# шрифты и отступы
+font_header = ('Arial', 15)
+font_entry = ('Arial', 12)
+label_font = ('Arial', 11)
+base_padding = {'padx': 10, 'pady': 8}
+header_padding = {'padx': 10, 'pady': 12}
+
+
+def data():
+    # функция нажатия кнопки заполнить данные
+    def data_clicked():
+        rez = open('logins.txt', 'a')
+        name = name_entry.get()
+        age = age_entry.get()
+        driving_experience = driving_experience_entry.get()
+        criminal = criminal_entry.get()
+        phone = phone_entry.get()
+        email = email_entry.get()
+        card  = card_entry.get()
+        s = ' ' + name + ' ' + age + ' ' + driving_experience + ' ' + criminal + ' ' + phone + ' ' + email + ' ' + card + '\n'
+        rez.write(s)
+        rez.close()
+        window_data.destroy()
+    header_padding_1 = {'padx': 10, 'pady': 7}
+    # главное окно приложения
+    window_data = Tk()
+    # заголовок окна
+    window_data.title('Регистрация')
+    # размер окна
+    window_data.geometry('450x330+400+150')
+    # менять размер
+    window_data.resizable(False, False)
+        
+    # настройка
+    data_label = Label(window_data, text='Заполнение данных', font=('Arial', 12), justify=CENTER, **header_padding_1)
+    data_label.grid(row=0, column=1)
+
+    # метка для имени
+    name_label = Label(window_data, text='Имя', font=('Arial', 12), **header_padding_1)
+    name_label.grid(row=1, column=0)
+
+    # поле имя
+    name_entry = Entry(window_data, bg='#fff', fg='#444', font=('Arial', 12))
+    name_entry.grid(row=1, column=1)
+
+    # метка возраста
+    age_label = Label(window_data, text='Возраст', font=('Arial', 12), **header_padding_1)
+    age_label.grid(row=2, column=0)
+
+    # возраст
+    age_entry = Entry(window_data, bg='#fff', fg='#444', font=('Arial', 12))
+    age_entry.grid(row=2, column=1)
+
+    # метка опыта вождения
+    driving_experience_label = Label(window_data, text='Опыт Вождения', font=('Arial', 12), **header_padding_1)
+    driving_experience_label.grid(row=3, column=0)
+
+    # опыт вождения
+    driving_experience_entry = Entry(window_data, bg='#fff', fg='#444', font=('Arial', 12))
+    driving_experience_entry.grid(row=3, column=1)
+
+    # метка Судимость
+    criminal_label = Label(window_data, text='Судимость', font=('Arial', 12), **header_padding_1)
+    criminal_label.grid(row=4, column=0)
+
+    # Судимость
+    criminal_entry = Entry(window_data, bg='#fff', fg='#444', font=('Arial', 12))
+    criminal_entry.grid(row=4, column=1)
+
+    # метка номер телефона
+    phone_label = Label(window_data, text='Номер телефона', font=('Arial', 12), **header_padding_1)
+    phone_label.grid(row=5, column=0)
+
+    # номер телефона
+    phone_entry = Entry(window_data, bg='#fff', fg='#444', font=('Arial', 12))
+    phone_entry.grid(row=5, column=1)
+
+    # метка E-mail
+    email_label = Label(window_data, text='E-mail', font=('Arial', 12), **header_padding_1)
+    email_label.grid(row=6, column=0)
+
+    # E-mail
+    email_entry = Entry(window_data, bg='#fff', fg='#444', font=('Arial', 12))
+    email_entry.grid(row=6, column=1)
+
+    # метка номер карты
+    card_label = Label(window_data, text='Card', font=('Arial', 12), **header_padding_1)
+    card_label.grid(row=7, column=0)
+
+    # номер карты
+    card_entry = Entry(window_data, bg='#fff', fg='#444', font=('Arial', 12))
+    card_entry.grid(row=7, column=1)
+
+    # кнопка Заполнить данные
+    send_btn = Button(window_data, text='Заполнить данные', command=data_clicked)
+    send_btn.grid(row=8, column=1)
+
+
+
 def reg():
     # главное окно приложения
     window_reg = Tk()
@@ -12,13 +114,6 @@ def reg():
     window_reg.geometry('450x230+400+150')
     # менять размер
     window_reg.resizable(False, False)
-
-    # шрифты и отступы
-    font_header = ('Arial', 15)
-    font_entry = ('Arial', 12)
-    label_font = ('Arial', 11)
-    base_padding = {'padx': 10, 'pady': 8}
-    header_padding = {'padx': 10, 'pady': 12}
 
     # кнопка Войти
     def clicked():
@@ -77,18 +172,19 @@ def reg():
                     line =  output.readline()
                     if not line:
                         break
-                    username_in_file, password_in_file = line.split() 
+                    username_in_file, password_in_file, name, age, driving_experience, criminal, phone, email, card = line.split() 
                     if (username == username_in_file):
                         flag = -1
                         messagebox.showerror('Ошибка','Такой логин уже занят')
                         window_reg.destroy()
                         reg() 
             if (flag == 0):     
-                s = (username+' '+password+"\n")
+                s = (username+' '+password)
                 output.write(s)
                 output.close()
                 window_reg.destroy()
                 break
+        data()
 
     # настройка
     main_label = Label(window_reg, text='Регистрация', font=font_header, justify=CENTER, **header_padding)
@@ -117,4 +213,4 @@ def reg():
 
     # главный цикл
     window_reg.mainloop()
-    
+    ent()
